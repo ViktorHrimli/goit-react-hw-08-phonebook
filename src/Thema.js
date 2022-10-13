811f2b9b30cc4ce61e5fe0e9be7fc2d33404839d
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2022-10-13 12:56:25 +0300

    main

---
+++ src/Thema.js
@@ -0,0 +1,46 @@
+export const theme = {
+  colors: {
+    black: '#000',
+    white: '#fff',
+    text: '#2a2a2a',
+    background: '#f7f7f7',
+    primary: 'green',
+    secondary: '#05a',
+    accent: '#00BFFF',
+    muted: 'red',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'system-ui, sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    sm: '24px',
+    l: '32px',
+    xl: '64px',
+  },
+  fontWeights: {
+    normal: 400,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+    bold: '2px solid',
+  },
+  radii: {
+    none: '0',
+    sm: '4px',
+    md: '8px',
+    lg: '16px',
+    round: '50%',
+  },
+};
